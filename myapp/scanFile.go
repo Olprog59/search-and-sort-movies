b22849b0323130b487a7d405527a0d2ca212a280
samuel michaux <kameleon836@gmail.com>
2017-10-11 11:24:11 +0200

    Change main_windows to flag_windows and same linux

---
+++ myapp/scanFile.go
@@ -0,0 +1,61 @@
+package myapp
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+func startScan(auto bool) {
+	if count, file := fileInFolder(); count > 0 {
+		if auto {
+			fmt.Println("Scan automatique")
+			go boucleFiles(file)
+		} else {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Println("Je vois qu'il y a des fichiers vidéos actuellement dans ton dossier source.")
+			fmt.Println("Veux tu faire le tri? (O/n)")
+			text, _ := reader.ReadString('\n')
+			fmt.Println(text)
+			if strings.TrimSpace(text) == "n" || strings.TrimSpace(text) == "N" {
+				return
+			}
+
+			go boucleFiles(file)
+		}
+	}
+}
+
+func fileInFolder() (int, []os.FileInfo) {
+	files, err := ioutil.ReadDir(GetEnv("dlna"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var count int
+	for _, f := range files {
+		if !f.IsDir() {
+			re := regexp.MustCompile(`(.mkv|.mp4|.avi|.flv)`)
+			if re.MatchString(filepath.Ext(f.Name())) {
+				count++
+			}
+		}
+	}
+	return count, files
+}
+
+func boucleFiles(files []os.FileInfo) {
+	log.Println("Démarrage du tri !")
+	for _, f := range files {
+		if !f.IsDir() {
+			log.Println("Movies : " + f.Name())
+			Process(f.Name())
+		}
+	}
+	log.Println("Tri terminé !")
+}
