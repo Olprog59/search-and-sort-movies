4d97b04b76565651868e34ea1797aca3f86f8946
samuel michaux <kameleon836@gmail.com>
2017-10-22 14:30:26 +0200

    Correct Bug Search Serie withyear in nama

--- myapp/movieDb_test.go
+++ myapp/movieDb_test.go
@@ -1,6 +1,8 @@
 package myapp
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_checkMovieDB(t *testing.T) {
 	type args struct {
@@ -22,13 +24,48 @@ func Test_checkMovieDB(t *testing.T) {
 				"transformers-the-last-knight",
 				[]string{"2017"},
 			},
-			"https://api.themoviedb.org/3/search/movie?api_key=ea8779638f078f25daa3913e80fe46eb&query=transformers-the-last-knight&primary_release_year=2017",
+			"https://api.themoviedb.org/3/search/movie?api_key=ea8779638f078f25daa3913e80fe46eb&query=transformers-the-last-knight&year=2017",
+		},
+		{
+			"checkMovieDB",
+			args{
+				true,
+				false,
+				"the-flash-2014",
+				[]string{"2014"},
+			},
+			"https://api.themoviedb.org/3/search/tv?api_key=ea8779638f078f25daa3913e80fe46eb&query=the-flash&first_air_date_year=2014",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := checkMovieDB(tt.args.tv, tt.args.lang, tt.args.name, tt.args.date...); got != tt.want {
 				t.Errorf("checkMovieDB() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_slugRemoveYearSerieForSearchMovieDB(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantNew string
+	}{
+		{
+			"slugRemoveYearSerieForSearchMovieDB", args{
+				"the-flash-2014",
+			},
+			"the-flash",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotNew := slugRemoveYearSerieForSearchMovieDB(tt.args.name); gotNew != tt.wantNew {
+				t.Errorf("slugRemoveYearSerieForSearchMovieDB() = %v, want %v", gotNew, tt.wantNew)
 			}
 		})
 	}
