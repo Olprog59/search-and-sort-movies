fca54bbec34bf6ff52b70a370dcad1b4f27d6679
samuel michaux <kameleon836@gmail.com>
2018-09-15 15:23:55 +0200

    Add flags for mac os x

---
+++ myapp/flag_darwin.go
@@ -0,0 +1,31 @@
+package myapp
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime"
+)
+
+func Flags(BuildName, BuildVersion, BuildHash, BuildDate, BuildClean string) {
+	vers := flag.Bool("v", false, "Indique la version de l'application")
+	scan := flag.Bool("scan", false, "Lancer le scan au démarrage de l'application")
+	flag.Parse()
+
+	if *vers {
+		// flag.PrintDefaults()
+		fmt.Printf("Name: %s\n", BuildName)
+		fmt.Printf("Version: %s\n", BuildVersion)
+		fmt.Printf("Git Commit Hash: %s\n", BuildHash)
+		fmt.Printf("Build Date: %s\n", BuildDate)
+		fmt.Printf("Built from clean source tree: %s\n", BuildClean)
+		fmt.Printf("OS: %s\n", runtime.GOOS)
+		fmt.Printf("Architecture: %s\n", runtime.GOARCH)
+		os.Exit(1)
+	}
+
+	if *scan {
+		startScan(true)
+	}
+
+}
