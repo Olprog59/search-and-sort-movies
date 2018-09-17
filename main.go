304aa5b73378eef9b0de03bf55ff9734baf18337
samuel michaux <kameleon836@gmail.com>
2018-09-17 09:57:23 +0200

    finish index and config. Remove config in console

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"path"
 	"runtime"
 	"search-and-sort-movies/myapp"
-	"strings"
+	"time"
 )
 
 var (
@@ -24,13 +20,10 @@ var (
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-
 }
 
 func main() {
-
 	myapp.Flags(BuildName, BuildVersion, BuildHash, BuildDate, BuildClean)
-
 	// Write log to file : log_SearchAndSort
 	f, err := os.OpenFile("log_SearchAndSort", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
@@ -39,96 +32,28 @@ func main() {
 	defer f.Close()
 	log.SetOutput(f)
 
-	// Start server web
 	myapp.StartServerWeb()
 
-	// Check if it's the first connection
-	if firstConnect() {
-		firstConfig()
-	} else {
-		for {
-			if myapp.GetEnv("dlna") == "" || myapp.GetEnv("movies") == "" || myapp.GetEnv("series") == "" {
-				firstConfig()
-			} else {
-				break
-			}
+	for {
+		if myapp.GetEnv("dlna") != "" || myapp.GetEnv("movies") != "" || myapp.GetEnv("series") != "" {
+			break
 		}
+		fmt.Println("En attente de configuration : va sur http://localhost:1515")
+		log.Println("En attente de configuration : va sur http://localhost:1515")
+		time.Sleep(30 * time.Second)
 	}
 
 	checkFolderExists(myapp.GetEnv("dlna"))
 	checkFolderExists(myapp.GetEnv("movies"))
 	checkFolderExists(myapp.GetEnv("series"))
 
-	fmt.Println("Start :-D")
-
-	fmt.Println("Ecoute sur le dossier : " + myapp.GetEnv("dlna"))
+	log.Println("Start :-D")
+	log.Println("Ecoute sur le dossier : " + myapp.GetEnv("dlna"))
 	myapp.Watcher(myapp.GetEnv("dlna"))
 
 }
-
-func firstConnect() bool {
-	_, err := os.Stat(".config.json")
-
-	if os.IsNotExist(err) {
-		log.Println(err)
-		return true
-	}
-	return false
-}
-
-func readJSONFileConsole() {
-	f, err := ioutil.ReadFile(".config.json")
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	fmt.Printf("%s\n", string(f))
-}
-
-func firstConfig() {
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Println("Hello, bienvenue sur l'application de tri des vidéos.")
-		fmt.Println("Ceci est ta première connexion donc il faut configurer des petites choses.")
-		pwd, _ := os.Getwd()
-		fmt.Println("A savoir, que tu te trouves dans le répertoire : " + pwd)
-		fmt.Println("Commençons par indiqué l'emplacement des fichiers à trier : (ex : /home/user/a_trier ou windows : C:\\Users\\Dupont\\Documents\\a_trier)")
-		text, _ := reader.ReadString('\n')
-		fmt.Println(text)
-		myapp.SetEnv("dlna", path.Clean(strings.TrimSpace(text)))
-		fmt.Println("Ensuite, il faut indiqué le dossier ou tu veux mettre tes films : (ex : /mnt/dlna/movies ou windows : F:\\films)")
-		text, _ = reader.ReadString('\n')
-		fmt.Println(text)
-		myapp.SetEnv("movies", path.Clean(strings.TrimSpace(text)))
-		fmt.Println("Pour finir, il faut indiqué le dossier ou tu veux mettre tes séries : (ex : /mnt/dlna/series ou windows : F:\\series)")
-		text, _ = reader.ReadString('\n')
-		fmt.Println(text)
-		myapp.SetEnv("series", path.Clean(strings.TrimSpace(text)))
-		fmt.Println("Pour la musique, il faut attendre les prochaines versions. :-(  ")
-
-		fmt.Println("Super. Voilà tout est configuré. On va vérifier le fichier : ")
-		fmt.Println('\n')
-		readJSONFileConsole()
-		fmt.Println("Est-ce que cela est correct? (O/n)")
-		text, _ = reader.ReadString('\n')
-		if strings.TrimSpace(text) == "n" || strings.TrimSpace(text) == "N" {
-			continue
-		} else {
-			break
-		}
-
-	}
-
-	fmt.Println("Cool!!! C'est parti. Enjoy")
-}
-
 func checkFolderExists(folder string) {
 	if _, err := os.Stat(folder); os.IsNotExist(err) {
 		os.MkdirAll(folder, os.ModePerm)
 	}
-}
-
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
 }
