18dda139847bb184253d8bcc0fb72426caef0d13
Samuel Michaux <olprog59@gmail.com>
2020-04-27 12:17:25 +0200

    mise à jour du regex du slugFile.go pour prendre encore plus de cas

--- myapp/slugFile.go
+++ myapp/slugFile.go
@@ -21,25 +21,33 @@ func (m *myFile) slugFile() {
 	video := regexp.MustCompile(`(?mi)-(french|dvdrip|multi|vostfr|dvd-r|bluray|bdrip|brrip|cam|ts|tc|vcd|md|ld|r[0-9]|xvid|divx|scr|dvdscr|repack|hdlight|720p|480p|1080p|2160p|uhd)`)
 	yearReg := regexp.MustCompile(`(?mi)-[0-9]{4}`)
 
-	serie := regexp.MustCompile(`(?mi)((s\d{1,2})(?:\W+)?(e?\d{1,2}))|(?:e\d{1,2})|(episode-(\d{2,3})-)`)
+	serie := regexp.MustCompile(`(?mi)((s\d{1,2})(?:\W+)?(e?\d{1,2}))|(?:e\d{1,2})|(episode-(\d{2,3})-)|((\d{1,2})-(\d{1,2}))|((saison|season)-(\d{1,2})-episode-(\d{1,2}))`)
+	//serie := regexp.MustCompile(`(?mi)((s\d{1,2})(?:\W+)?(e?\d{1,2}))|(?:e\d{1,2})|(episode-(\d{2,3})-)`)
 	match := serie.FindAllStringSubmatch(m.name, -1)
 
 	if len(match) > 0 {
 		for _, v := range match {
-			if v[2] == "" && v[4] == "" {
-				m.episode = formatSaisonNumberOuEpisode(v[0], 'e')
+			if v[7] != "" && v[8] != "" {
+				m.season = formatSaisonNumberOuEpisode(v[7], 's')
+				m.episode = formatSaisonNumberOuEpisode(v[8], 'e')
+			} else if v[11] != "" && v[12] != "" {
+				m.season = formatSaisonNumberOuEpisode(v[11], 's')
+				m.episode = formatSaisonNumberOuEpisode(v[12], 'e')
 			} else if v[5] != "" {
 				m.season = "s00"
 				m.episode = "e" + v[5]
-			} else {
+			} else if v[2] != "" && v[3] != "" {
 				m.season = formatSaisonNumberOuEpisode(v[2], 's')
 				m.episode = formatSaisonNumberOuEpisode(v[3], 'e')
+
+			} else if v[2] == "" && v[4] == "" {
+				m.episode = formatSaisonNumberOuEpisode(v[0], 'e')
 			}
 			break
 		}
 		m.serieNumber = fmt.Sprintf("%s%s", m.season, m.episode)
 		if len(serie.FindStringIndex(m.name)) > 0 {
-			m.serieName = m.complete[:serie.FindStringIndex(m.name)[0]-1]
+			m.serieName = slugify.Slugify(m.complete[:serie.FindStringIndex(m.name)[0]-1])
 			if len(yearReg.FindStringIndex(m.serieName)) > 0 {
 				m.year, err = strconv.Atoi(yearReg.FindString(m.serieName)[1:])
 				if err != nil {
@@ -83,6 +91,8 @@ func (m *myFile) slugFile() {
 func formatSaisonNumberOuEpisode(num string, seasonOrEpisode byte) string {
 	if len(num) == 2 && (num[0] == 's' || num[0] == 'e') {
 		num = fmt.Sprintf("%s0%s", string(num[0]), string(num[1]))
+	} else if len(num) == 1 {
+		num = fmt.Sprintf("%s0%s", string(seasonOrEpisode), string(num[0]))
 	} else if num[0] != seasonOrEpisode {
 		num = fmt.Sprintf("%s%s", string(seasonOrEpisode), num)
 	}
