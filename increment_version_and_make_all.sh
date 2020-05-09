d893c314797836a2d7402200de98b11d1e253349
Samuel Michaux <olprog59@gmail.com>
2020-05-09 12:34:54 +0200

    Update fichier sh

--- increment_version_and_make_all.sh
+++ increment_version_and_make_all.sh
@@ -2,11 +2,11 @@
 
 # Test de nos fichiers test
 ALL_TEST=$(go test ./... -v)
-COUNT_FAIL=$(echo $ALL_TEST | grep -o 'FAIL' | wc -l | tr -d '[:space:]')
-if [ $COUNT_FAIL = 0 ]
+COUNT_FAIL=$(echo "$ALL_TEST" | grep -o 'FAIL' | wc -l | tr -d '[:space:]')
+if [ "$COUNT_FAIL" = 0 ]
 then
     GIT_STATUS=$(git status --porcelain | wc -l | tr -d '[:space:]')
-    if [ $GIT_STATUS = 0 ]
+    if [ "$GIT_STATUS" = 0 ]
     then
       BUILD_CLEAN=yes
     else
@@ -19,12 +19,12 @@ then
         # Increment version
         VERSION=$(cat 'VERSION')
         NEW_VERSION="${VERSION%.*}.$((${VERSION##*.}+1))"
-        printf $NEW_VERSION > 'VERSION'
+        print "$NEW_VERSION" > 'VERSION'
 
         git add VERSION && git commit -m "VERSION -> $NEW_VERSION"
 
         GIT_STATUS=$(git status --porcelain | wc -l | tr -d '[:space:]')
-        if [ $GIT_STATUS = 0 ]
+        if [ "$GIT_STATUS" = 0 ]
         then
           BUILD_CLEAN=yes
         else
