f363b98c2b6ca032b33649f1ee202bba1ff670c4
Samuel Michaux <olprog59@gmail.com>
2020-05-09 12:30:57 +0200

    Update fichier sh

--- increment_version_and_make_all.sh
+++ increment_version_and_make_all.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Test de nos fichiers test
 ALL_TEST=$(go test ./... -v)
 COUNT_FAIL=$(echo $ALL_TEST | grep -o 'FAIL' | wc -l | tr -d '[:space:]')
@@ -18,11 +20,22 @@ then
         VERSION=$(cat 'VERSION')
         NEW_VERSION="${VERSION%.*}.$((${VERSION##*.}+1))"
         printf $NEW_VERSION > 'VERSION'
-        $(git add VERSION && git -commit "VERSION -> $NEW_VERSION")
-
-        # Make all
-        make darwin && make linux && make linux
-        echo "Je pense que tout est correct"
+        $(git add VERSION && git commit -m "VERSION -> $NEW_VERSION")
+        GIT_STATUS=$(git status --porcelain | wc -l | tr -d '[:space:]')
+        if [ $GIT_STATUS = 0 ]
+        then
+          BUILD_CLEAN=yes
+        else
+          BUILD_CLEAN=no
+        fi
+        if [ $BUILD_CLEAN = yes ]
+        then
+            # Make all
+            make darwin && make linux && make linux
+            echo "Je pense que tout est correct"
+        else
+            echo "Problème de commit juste après un changement de version. A vérifier"
+        fi
     else
         echo "Tu as oublié de commit et faire le push"
     fi
