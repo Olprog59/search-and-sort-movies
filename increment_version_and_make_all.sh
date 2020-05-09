8f26f681248518ee68d51ed77ed9b05b589545b9
Samuel Michaux <olprog59@gmail.com>
2020-05-09 13:31:52 +0200

    Ajout d'un flag pour mettre à jour la version en Major-minor-patch-special increment_version_and_make_all.sh

--- increment_version_and_make_all.sh
+++ increment_version_and_make_all.sh
@@ -3,6 +3,75 @@
 # Test de nos fichiers test
 ALL_TEST=$(go test ./... -v)
 COUNT_FAIL=$(echo "$ALL_TEST" | grep -o 'FAIL' | wc -l | tr -d '[:space:]')
+if [ $# = 0 ]
+then
+    echo "N'oublie pas de mettre un premier paramètre. Ex: "
+    printf "\t-M (Major) : 0.6.3.2 -> 1.0.0.0\n"
+    printf "\t-m (minor) : 0.6.3.2 -> 0.7.0.0\n"
+    printf "\t-p (patch) : 0.6.3.2 -> 0.6.4.0\n"
+    printf "\t-s (special when beta) : 0.6.3.2 -> 0.6.4.3\n"
+    exit;
+fi
+
+function increment() {
+
+  while getopts ":Mmps" Option
+  do
+    case $Option in
+      M ) major=true;;
+      m ) minor=true;;
+      p ) patch=true;;
+      s ) special=true;;
+    esac
+  done
+
+  shift $(($OPTIND - 1))
+
+  version=$1
+
+  # Build array from version string.
+
+  a=( ${version//./ } )
+
+  # If version string is missing or has the wrong number of members, show usage message.
+
+  if [ ${#a[@]} -ne 4 ]
+  then
+    echo "usage: $(basename $0) [-Mmps] Major.minor.patch.special"
+    exit 1
+  fi
+
+  # Increment version numbers as requested.
+
+  if [ ! -z $major ]
+  then
+    ((a[0]++))
+    a[1]=0
+    a[2]=0
+    a[3]=0
+  fi
+
+  if [ ! -z $minor ]
+  then
+    ((a[1]++))
+    a[2]=0
+    a[3]=0
+  fi
+
+  if [ ! -z $patch ]
+  then
+    ((a[2]++))
+    a[3]=0
+  fi
+
+  if [ ! -z $special ]
+  then
+    ((a[3]++))
+  fi
+
+  NEW_VERSION="${a[0]}.${a[1]}.${a[2]}.${a[3]}"
+}
+
 if [ "$COUNT_FAIL" = 0 ]
 then
     GIT_STATUS=$(git status --porcelain | wc -l | tr -d '[:space:]')
@@ -18,7 +87,10 @@ then
         echo "commit ok"
         # Increment version
         VERSION=$(cat 'VERSION')
-        NEW_VERSION="${VERSION%.*}.$((${VERSION##*.}+1))"
+
+        increment "$1" "$VERSION" NEW_VERSION
+
+#        NEW_VERSION="${VERSION%.*}.$((${VERSION##*.}+1))"
 
         echo "Attention, la version a changé en ${NEW_VERSION}. Souhaites-tu continuer à commit puis make le project?"
         select yn in "Yes" "No"; do
