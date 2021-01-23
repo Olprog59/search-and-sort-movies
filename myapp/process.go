d2c3d731488f34624bc775616a228aa616b58b4b
Samuel Michaux <olprog59@gmail.com>
2021-01-24 00:13:42 +0100

    test de exec pour move file au lieu de MoveFile avec copy car plus long. Retour à la ligne pour les log

--- myapp/process.go
+++ myapp/process.go
@@ -2,10 +2,8 @@ package myapp
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/Machiel/slugify"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -13,7 +11,6 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
-	"runtime"
 	"search-and-sort-movies/myapp/constants"
 	"search-and-sort-movies/myapp/logger"
 	"strconv"
@@ -46,7 +43,7 @@ func (m *myFile) Process() {
 	m.count = 0
 	_, m.complete = filepath.Split(m.file)
 	m.fileWithoutDir = m.complete
-	fmt.Println("complete: ", m.complete)
+	fmt.Println(logger.Info("complete: ", m.complete))
 	m.start("")
 }
 
@@ -61,7 +58,7 @@ func (m *myFile) start(serieOrMovieOrBoth string) {
 
 func (m *myFile) isMovie() {
 	extension := filepath.Ext(m.file)
-	fmt.Println("name: ", m.name)
+	fmt.Println(logger.Info("name: ", m.name))
 	var movie MoviesDb
 	if m.transName != "" {
 		movie, _ = m.dbMovies(false, m.transName)
@@ -79,17 +76,14 @@ func (m *myFile) isMovie() {
 		} else {
 			path1 = movies + string(os.PathSeparator) + m.complete
 		}
-		if runtime.GOOS == "windows" {
-			copyFile(m.file, movies+string(os.PathSeparator)+path1)
+
+		if moveOrRenameFile(m.file, path1) {
+			fmt.Println(logger.Info("%s a bien été déplacé dans %s", m.fileWithoutDir, path1))
 			m.createFileForLearning(true)
-		} else {
-			if moveOrRenameFile(m.file, path1) {
-				fmt.Printf("%s a bien été déplacé dans %s", m.fileWithoutDir, path1)
-				m.createFileForLearning(true)
-			}
 		}
+
 	} else {
-		fmt.Printf("isMovie : %s", m.name)
+		fmt.Println(logger.Warn("isMovie : %s", m.name))
 		m.isNotFindInMovieDb(m.name, "movie")
 	}
 }
@@ -117,7 +111,7 @@ func (m *myFile) isNotFindInMovieDb(name, serieOrMovie string) {
 		m.translateName()
 		m.start(serieOrMovie)
 	} else {
-		fmt.Println(name + ", n'a pas été trouvé sur https://www.themoviedb.org/search?query=" + name + ".\n Test manuellement si tu le trouves ;-)")
+		fmt.Println(logger.Warn(name + ", n'a pas été trouvé sur https://www.themoviedb.org/search?query=" + name + ".\n Test manuellement si tu le trouves ;-)"))
 		m.createFileForLearning(false)
 		m.count = 0
 	}
@@ -128,24 +122,19 @@ func (m *myFile) checkFolderSerie() (string, string) {
 	newFolder := string(os.PathSeparator) + m.serieName + string(os.PathSeparator) + "season-" + m.season[1:]
 	folderOk := series + string(os.PathSeparator) + m.serieName
 	if _, err := os.Stat(folderOk); os.IsNotExist(err) {
-		fmt.Printf("Création du dossier : %s\n", m.serieName)
+		fmt.Printf(logger.Info("Création du dossier : %s", m.serieName))
 		createFolder(folderOk)
 	}
 	if _, err := os.Stat(series + newFolder); os.IsNotExist(err) {
-		fmt.Printf("Création du dossier : %s\n", newFolder)
+		fmt.Println(logger.Info("Création du dossier : %s", newFolder))
 		createFolder(series + newFolder)
 	}
 
 	finalFilePath := series + newFolder + string(os.PathSeparator) + m.complete
 
-	if runtime.GOOS == "windows" {
-		copyFile(m.file, finalFilePath)
+	if moveOrRenameFile(m.file, finalFilePath) {
+		fmt.Println(logger.Info("%s a bien été déplacé dans %s", m.fileWithoutDir, finalFilePath))
 		m.createFileForLearning(true)
-	} else {
-		if moveOrRenameFile(m.file, finalFilePath) {
-			fmt.Printf("%s a bien été déplacé dans %s", m.fileWithoutDir, finalFilePath)
-			m.createFileForLearning(true)
-		}
 	}
 	return m.complete, finalFilePath
 }
@@ -168,12 +157,12 @@ func (m *myFile) translateName() {
 	fmt.Println(resp.Request)
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("response status code was %d\n", resp.StatusCode)
+		fmt.Println(logger.Warn("response status code was %d\n", resp.StatusCode))
 	}
 
 	ctype := resp.Header.Get("Content-Type")
 	if !strings.HasPrefix(ctype, "application/json") {
-		fmt.Printf("response content type was %s not text/html\n", ctype)
+		fmt.Println(logger.Warn("response content type was %s not text/html\n", ctype))
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
@@ -204,9 +193,10 @@ func moveOrRenameFile(filePathOld, filePathNew string) bool {
 	//err := os.Rename(filePathOld, filePathNew)
 	//err := MoveFile(filePathOld, filePathNew)
 	cmd := exec.Command("/bin/sh", "-c", "mv "+filePathOld+" "+filePathNew)
+	fmt.Println(logger.Info("Test du mv avec exec.Command"))
 	err := cmd.Run()
 	if err != nil {
-		fmt.Printf("Move Or Rename File : %s", err)
+		fmt.Println(logger.Warn("Move Or Rename File : %s", err))
 		mu.Unlock()
 		return false
 	}
@@ -216,128 +206,17 @@ func moveOrRenameFile(filePathOld, filePathNew string) bool {
 		if len(file) == 0 {
 			err = watch.Remove(folder)
 			if err != nil {
-				fmt.Println("Erreur sur la suppression du watcher sur le dossier : ", folder)
+				fmt.Println(logger.Warn("Erreur sur la suppression du watcher sur le dossier : ", folder))
 			}
-			fmt.Println("Suppression du watcher sur le dossier : ", folder)
+			fmt.Println(logger.Info("Suppression du watcher sur le dossier : ", folder))
 			err := os.Remove(folder)
 			if err != nil {
-				fmt.Println("Erreur de suppression de dossier : ", folder)
+				fmt.Println(logger.Warn("Erreur de suppression de dossier : ", folder))
 			}
 		}
 	}
 	mu.Unlock()
 	return true
-}
-
-//func MoveFile(sourcePath, destPath string) error {
-//	inputFile, err := os.Open(sourcePath)
-//	if err != nil {
-//		fmt.Println(logger.Fata("Couldn't open source file: ", err))
-//		return err
-//	}
-//	outputFile, err := os.Create(destPath)
-//	if err != nil {
-//		inputFile.Close()
-//		fmt.Println(logger.Fata("Couldn't open dest file: ", err))
-//		return err
-//	}
-//	defer outputFile.Close()
-//	_, err = io.Copy(outputFile, inputFile)
-//	inputFile.Close()
-//	if err != nil {
-//		fmt.Println(logger.Fata("Writing to output file failed: ", err))
-//		return err
-//	}
-//	// The copy was successful, so now delete the original file
-//	err = os.Remove(sourcePath)
-//	if err != nil {
-//		fmt.Println(logger.Fata("Failed removing original file: ", err))
-//		return err
-//	}
-//	return nil
-//}
-
-// si l'OS est windows alors je fais une copie et pas un déplacement
-func copyFile(oldFile, newFile string) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		r, err := os.Open(oldFile)
-		if err != nil {
-			fmt.Println(logger.Warn(err))
-		}
-		defer r.Close()
-
-		defer wg.Done()
-
-		w, err := os.Create(newFile)
-		if err != nil {
-			fmt.Println(logger.Warn(err))
-		}
-		defer w.Close()
-
-		// do the actual work
-		n, err := io.Copy(w, r)
-		if err != nil {
-			fmt.Println(logger.Warn(err))
-		}
-		err = os.Chown(newFile, 0, 0)
-		if err != nil {
-			fmt.Println(logger.Warn(err))
-		}
-		fmt.Printf("Copied file : %s to %s - %v bytes\n", oldFile, newFile, n)
-		return
-	}()
-
-	wg.Wait()
-	go func() {
-		err := checkIfSizeIsSame(oldFile, newFile)
-		if err != nil {
-			fmt.Println(logger.Warn(err))
-			copyFile(oldFile, newFile)
-		} else {
-			fmt.Println("Le fichier est correctement copié. La source va être supprimé !")
-			removeAfterCopy(oldFile)
-		}
-	}()
-}
-
-func checkSizeFile(file string) (int64, error) {
-	f, err := os.Stat(file)
-	if err != nil {
-		fmt.Println(logger.Warn(err))
-		return f.Size(), err
-	}
-
-	return f.Size(), err
-}
-
-func checkIfSizeIsSame(oldFile, newFile string) error {
-	newFileSize, err := checkSizeFile(newFile)
-	if err != nil {
-		return err
-	}
-
-	oldFileSize, err := checkSizeFile(oldFile)
-	if err != nil {
-		return err
-	}
-
-	if newFileSize != oldFileSize {
-		return errors.New("the files are not the same size. The operation will start again")
-	}
-	return nil
-}
-
-func removeAfterCopy(oldFile string) {
-	go func() {
-		time.Sleep(time.Second * 10)
-		fmt.Println("Sous windows, suppression du fichier / folder : ", oldFile)
-		err := os.Remove(oldFile)
-		if err != nil {
-			fmt.Println(logger.Warn(err))
-		}
-	}()
 }
 
 // création d'un fichier pour le learning
